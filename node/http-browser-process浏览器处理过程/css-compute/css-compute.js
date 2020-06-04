cbf8d44a05ad3afa39e0e77833ec4d4c732dce2d
Siwen19 <59066429+Siwen19@users.noreply.github.com>
2020-06-04 20:35:34 +0800

    Update css-compute.js

--- node/http-browser-process浏览器处理过程/css-compute/css-compute.js
+++ node/http-browser-process浏览器处理过程/css-compute/css-compute.js
@@ -1,30 +1,81 @@
 const css = require('css');
 let htmlStr = `<html>
   <head></head>
-  <body>
-    <img a="a" b="b"/>
-    <span></span>
+  <body class="parent">
+    <img src="a" alt="b"/>
+    <span class="paint"></span>
     <div class="cls" id="myid"></div>
   </body> 
 </html>`
 let cssStr = `
-.cls {
+.parent .cls {
     font-size: 16px;
 }
 #myid {
     background-color: red;
 }
+.paint {
+    color: blue;
+}
 `
 let rules = css.parse(cssStr).stylesheet.rules;
-
-console.log(JSON.stringify(rules, null, 2));
+// console.log(JSON.stringify(rules, null, 2));
 let currentToken = null;
 let currentAttribute = null;
 let stack = [{ type: 'document', children: [] }];
 parse(htmlStr);
 console.log(JSON.stringify(stack[0], null, 2));
-function computeCss(ele) {
-    // 计算符合这个元素的所有css规则，css规则应用到这个节点上
+
+function match(selector, ele) {
+    if (!selector || !ele.attributes) {
+        return false;
+    }
+    // #myid
+    // {name: id, value：‘myid’}
+    if (selector.charAt(0) === '#') {
+        let idAttr = ele.attributes.find(e => e.name === 'id');
+        if (idAttr && idAttr.value === selector.replace('#', '')) return true;
+    } else if (selector.charAt(0) === '.') {
+        let classAttr = ele.attributes.find(e => e.name === 'class');
+        if (classAttr && classAttr.value === selector.replace('.', '')) return true;
+    } else {
+        if (ele.tagName === selector) return true;
+    }
+    return false;
+}
+function computerCss(ele) {
+    // 计算 符合这个 ele 的所有 css 规则 || css 规则 应用到这个节点上面
+    // 1: 靠 ele 属性 父节点，和 css 里面 选择器 匹配
+    // 2：匹配 从后往前匹配  .parent .cls
+    // tagName #id  .className
+    // .parent .cls
+    // div  || div .cls  || span #parentID .parent #id
+    let elements = stack.slice(0).reverse();
+    if (!ele.computerStyle) ele.computerStyle = {};
+    // 所有 css 规则
+    for (let rule of rules) {
+        let selector = rule.selectors[0].split(' ').reverse();
+        // 最后一项匹配上了
+        if (!match(selector[0], ele)) {
+            // 跳过本轮循环，往后的步骤
+            continue;
+        }
+        // 看父级满不满足  
+        // [{type: 'doc'}, {html}, [header]]
+        // [#id .parent  #parentID  span]
+        let j = 1;
+        for (let i = 0; i < elements.length; i++) {
+            if (match(selector[j], elements[i])) j++
+        }
+        // 匹配
+        if (j >= selector.length) {
+            // rule rule css 规则添加到 ele
+            for (let delecare of rule.declarations) {
+                const { property, value } = delecare;
+                ele.computerStyle[property] = value
+            }
+        }
+    }
 
 }
 function emit(token) {
@@ -39,7 +90,7 @@ function emit(token) {
             tagName: token.tagName
         }
         // 知道 attributes， 知道了element父节点
-        computeCss(element);
+        computerCss(element);
         stack.push(element);
         // 作为栈顶的元素子节点，为了生成树
         // if (!top.children) top.children = [];
