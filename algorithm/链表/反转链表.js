dcad22d4f9a5399842170a438ed8cb33af39ea1b
Siwen19 <59066429+Siwen19@users.noreply.github.com>
2020-07-05 23:02:25 +0800

    反转链表 js

--- algorithm/链表/反转链表.js
+++ algorithm/链表/反转链表.js
@@ -1,1 +1,14 @@
-
+var reverseList = function(head) {   
+    // 暂存 指向 归位
+    let pre = null;
+    let cur = head; 
+    while (cur !== null) {
+        let temp = cur.next;
+        // 指向
+        cur.next = pre;
+        // 归位
+        pre = cur;
+        cur = temp;
+    }
+    return pre;
+};
