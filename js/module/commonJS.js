496eee5bb4dab00df201131054aa08f449c14044
Siwen19 <59066429+Siwen19@users.noreply.github.com>
2020-07-10 21:14:11 +0800

    Add files via upload

---
+++ js/module/commonJS.js
@@ -0,0 +1,11 @@
+if (Math.random() > 0.5) {
+    const http = require('http');
+}
+
+function foo() {}
+let arr = []
+exports = function() {}
+exports = 1
+exports = {}
+exports = '123'
+console.log(typeof exports)
