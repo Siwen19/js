6add3d344f60751438186da9073c5fc8b74f53b3
Siwen19 <59066429+Siwen19@users.noreply.github.com>
2020-07-10 21:15:44 +0800

    Add files via upload

---
+++ js/module/output.js
@@ -0,0 +1,13 @@
+(function () {
+    'use strict';
+
+    const add = (a, b) => {
+        return a + b;
+    };
+
+    // 只用到了 add, 为了优化代码体积，可以直接删除 minus rollup(打包工具)
+    let sum = add(1, 2);
+    let sum1 = sum + 2;
+    console.log(sum1);
+
+}());
